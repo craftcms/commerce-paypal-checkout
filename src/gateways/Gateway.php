1261962820ff9c00b2c56ad4a0aa61530cf6cc66
Luke Holder <lukemh@gmail.com>
2021-12-06 10:52:50 +0800

    1.3.0.1 wont exist

--- src/gateways/Gateway.php
+++ src/gateways/Gateway.php
@@ -207,7 +207,7 @@ class Gateway extends BaseGateway
      *
      * @param bool $parse Whether to parse the value as an environment variable
      * @return string|null
-     * @since 1.3.0.1
+     * @since 1.3.1
      */
     public function getTestMode(bool $parse = true)
     {
@@ -218,7 +218,7 @@ class Gateway extends BaseGateway
      * Sets whether Test Mode should be used.
      *
      * @param string|bool $testMode
-     * @since 1.3.0.1
+     * @since 1.3.1
      */
     public function setTestMode($testMode): void
     {
@@ -230,7 +230,7 @@ class Gateway extends BaseGateway
      *
      * @param bool $parse Whether to parse the value as an environment variable
      * @return string|null
-     * @since 1.3.0.1
+     * @since 1.3.1
      */
     public function getSendCartInfo(bool $parse = true)
     {
@@ -241,7 +241,7 @@ class Gateway extends BaseGateway
      * Sets whether cart information should be sent to the payment gateway.
      *
      * @param string|bool $sendCartInfo
-     * @since 1.3.0.1
+     * @since 1.3.1
      */
     public function setSendCartInfo($sendCartInfo): void
     {
